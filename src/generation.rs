b5abc855b4c7765d0a176df7d2e152230c9a7681
Elijah Potter <me@elijahpotter.dev>
2026-01-29 14:03:16 -0700

    fix: generation maybe?

--- src/generation.rs
+++ src/generation.rs
@@ -2,16 +2,18 @@ use burn::Tensor;
 use burn::prelude::Backend;
 
 use crate::model::Model;
-use crate::tokenizer::{self, indices_to_text, text_to_indices};
+use crate::tokenizer::{VOCAB_SIZE, indices_to_text, text_to_indices};
 
 pub fn generate<B: Backend>(model: &Model<B>, context: &str) {
     let chars: Vec<_> = context.chars().collect();
 
     let input = text_to_indices(&chars, &model.device());
+    let [batch_size, seq_len] = input.dims();
 
     let output = model.forward(input);
+    let output = output.reshape([batch_size, seq_len, VOCAB_SIZE]);
 
-    let indices = output.argmax(1);
+    let indices = output.argmax(2).reshape([batch_size, seq_len]);
     let text = indices_to_text(indices);
 
     let text_str: String = text.into_iter().collect();

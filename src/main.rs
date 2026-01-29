aa12547cdc587022941a876ef236fd5e1d9304a9
Elijah Potter <me@elijahpotter.dev>
2026-01-29 08:36:45 -0700

    feat: use clap for training

--- src/main.rs
+++ src/main.rs
@@ -3,88 +3,56 @@ mod batcher;
 mod dataset;
 mod model;
 mod tokenizer;
+mod training;
 
-use crate::batcher::BatchItem;
-use burn::backend::{Autodiff, NdArray};
-use burn::train::metric::CudaMetric;
-use burn::train::{InferenceStep, Learner};
-use burn::{
-    backend::Wgpu,
-    data::{dataloader::DataLoaderBuilder, dataset::transform::SamplerDataset},
-    lr_scheduler::noam::NoamLrSchedulerConfig,
-    nn::{
-        Dropout, DropoutConfig, Embedding, EmbeddingConfig, Relu,
-        loss::CrossEntropyLossConfig,
-        transformer::{TransformerEncoder, TransformerEncoderConfig, TransformerEncoderInput},
-    },
-    optim::{AdamConfig, decay::WeightDecayConfig},
-    prelude::*,
-    record::CompactRecorder,
-    tensor::backend::AutodiffBackend,
-    train::{
-        ClassificationOutput, SupervisedTraining, TrainOutput, TrainStep,
-        metric::{AccuracyMetric, LearningRateMetric, LossMetric, PerplexityMetric},
-    },
-};
-use log::info;
+use std::path::PathBuf;
+
+use burn::module::Module;
+use burn::record::CompactRecorder;
+use clap::Parser;
 
 use self::model::ModelConfig;
-use self::{batcher::GenBatcher, dataset::FileFolderDataset};
+
+#[derive(Parser, Debug)]
+enum Command {
+    Train {
+        train_data: PathBuf,
+        test_data: PathBuf,
+        dropout: f64,
+        /// The total number of transformer blocks.
+        transformer_blocks: usize,
+        embed_dims: usize,
+        /// The number of attention heads to use in the transformer.
+        attn_heads: usize,
+        /// The size of the perceptrons between the attention blocks.
+        percept_size: usize,
+        save_to: PathBuf,
+    },
+}
 
 fn main() {
-    const VOCAB_SIZE: usize = 64;
-    const MAX_SEQ_LEN: usize = 64;
-    type TargetBackend = Autodiff<Wgpu<f32, i32>>;
+    let command = Command::parse();
 
-    let device = Default::default();
-    let model = ModelConfig::new(6, 256, 8, 1024)
-        .with_dropout(0.1)
-        .init::<TargetBackend>(&device);
+    match command {
+        Command::Train {
+            train_data,
+            test_data,
+            dropout,
+            transformer_blocks,
+            embed_dims,
+            attn_heads,
+            percept_size,
+            save_to,
+        } => {
+            let model = training::train(
+                ModelConfig::new(transformer_blocks, embed_dims, attn_heads, percept_size)
+                    .with_dropout(dropout),
+                train_data,
+                test_data,
+            );
 
-    let dataset_train = FileFolderDataset::load_from_folder("./train");
-    let dataset_test = FileFolderDataset::load_from_folder("./test");
-
-    let dataloader_train = DataLoaderBuilder::new(GenBatcher)
-        .batch_size(32)
-        .num_workers(4)
-        .build(SamplerDataset::new(dataset_train, 10_000));
-
-    let dataloader_test = DataLoaderBuilder::new(GenBatcher)
-        .batch_size(32)
-        .num_workers(4)
-        .build(SamplerDataset::new(dataset_test, 1000));
-
-    let optim = AdamConfig::new()
-        .with_weight_decay(Some(WeightDecayConfig::new(1.0e-6)))
-        .init();
-
-    let accum = 6;
-
-    let lr_scheduler = NoamLrSchedulerConfig::new(0.01 / accum as f64)
-        .with_warmup_steps(6000)
-        .with_model_size(512)
-        .init()
-        .unwrap();
-
-    let training = SupervisedTraining::new("./checkpoints", dataloader_train, dataloader_test)
-        .metric_train(CudaMetric::new())
-        .metric_valid(CudaMetric::new())
-        .metric_train_numeric(AccuracyMetric::new().with_pad_token(0))
-        .metric_valid_numeric(AccuracyMetric::new().with_pad_token(0))
-        .metric_train_numeric(PerplexityMetric::new().with_pad_token(0))
-        .metric_valid_numeric(PerplexityMetric::new().with_pad_token(0))
-        .metric_train_numeric(LossMetric::new())
-        .metric_valid_numeric(LossMetric::new())
-        .metric_train_numeric(LearningRateMetric::new())
-        .with_file_checkpointer(CompactRecorder::new())
-        .grads_accumulation(accum)
-        .num_epochs(128)
-        .summary();
-
-    info!("Selected device: {:?}", device);
-
-    let result = training.launch(Learner::new(model, optim, lr_scheduler));
-
-    // let recorder = CompactRecorder::new();
-    // result.model.save_file("model", &recorder).unwrap();
+            let recorder = CompactRecorder::new();
+            model.save_file(save_to, &recorder).unwrap();
+        }
+    }
 }
---
+++ src/training.rs
@@ -0,0 +1,75 @@
+use std::path::Path;
+
+use burn::backend::{Autodiff, Wgpu};
+use burn::data::dataloader::DataLoaderBuilder;
+use burn::data::dataset::transform::SamplerDataset;
+use burn::lr_scheduler::noam::NoamLrSchedulerConfig;
+use burn::optim::AdamConfig;
+use burn::optim::decay::WeightDecayConfig;
+use burn::record::CompactRecorder;
+use burn::train::metric::{
+    AccuracyMetric, CudaMetric, LearningRateMetric, LossMetric, PerplexityMetric,
+};
+use burn::train::{Learner, SupervisedTraining};
+use log::info;
+
+use crate::batcher::GenBatcher;
+use crate::dataset::FileFolderDataset;
+use crate::model::{Model, ModelConfig};
+
+type TargetBackend = Autodiff<Wgpu<f32, i32>>;
+pub type ReturnBackend = Wgpu<f32, i32>;
+
+pub fn train(
+    m: ModelConfig,
+    train_folder: impl AsRef<Path>,
+    test_folder: impl AsRef<Path>,
+) -> Model<ReturnBackend> {
+    let device = Default::default();
+    let model = m.init::<TargetBackend>(&device);
+
+    let dataset_train = FileFolderDataset::load_from_folder(train_folder);
+    let dataset_test = FileFolderDataset::load_from_folder(test_folder);
+
+    let dataloader_train = DataLoaderBuilder::new(GenBatcher)
+        .batch_size(32)
+        .num_workers(4)
+        .build(SamplerDataset::new(dataset_train, 10_000));
+
+    let dataloader_test = DataLoaderBuilder::new(GenBatcher)
+        .batch_size(32)
+        .num_workers(4)
+        .build(SamplerDataset::new(dataset_test, 1000));
+
+    let optim = AdamConfig::new()
+        .with_weight_decay(Some(WeightDecayConfig::new(1.0e-6)))
+        .init();
+
+    let accum = 6;
+
+    let lr_scheduler = NoamLrSchedulerConfig::new(0.01 / accum as f64)
+        .with_warmup_steps(6000)
+        .with_model_size(512)
+        .init()
+        .unwrap();
+
+    let training = SupervisedTraining::new("./checkpoints", dataloader_train, dataloader_test)
+        .metric_train(CudaMetric::new())
+        .metric_valid(CudaMetric::new())
+        .metric_train_numeric(AccuracyMetric::new().with_pad_token(0))
+        .metric_valid_numeric(AccuracyMetric::new().with_pad_token(0))
+        .metric_train_numeric(PerplexityMetric::new().with_pad_token(0))
+        .metric_valid_numeric(PerplexityMetric::new().with_pad_token(0))
+        .metric_train_numeric(LossMetric::new())
+        .metric_valid_numeric(LossMetric::new())
+        .metric_train_numeric(LearningRateMetric::new())
+        .with_file_checkpointer(CompactRecorder::new())
+        .grads_accumulation(accum)
+        .num_epochs(128)
+        .summary();
+
+    info!("Selected device: {:?}", device);
+
+    let result = training.launch(Learner::new(model, optim, lr_scheduler));
+    result.model
+}

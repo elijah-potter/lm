d4abf90005bbdefd527f02f3917ebe30f33d08ac
Elijah Potter <me@elijahpotter.dev>
2026-03-04 08:53:56 -0700

    fix: permission problems on Runpod

--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
-tar -xzf ./train.tar.gz
-tar -xzf ./test.tar.gz
+tar -xzf ./train.tar.gz --no-same-owner
+tar -xzf ./test.tar.gz --no-same-owner
+
 RUSTFLAGS="-C target-cpu=native" cargo run --release -- train train test 0.1 0.8 24 256 8 1024 48 model.mpk

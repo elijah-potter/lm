2d0a527aa013913b0d9a0caecc99f7a0fd3f6ca9
Elijah Potter <me@elijahpotter.dev>
2026-09-01 09:50:08 -0600

    fix: adjust to new embedding size

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-RUSTFLAGS="-C target-cpu=native" cargo run --release -- train train.tar.gz test.tar.gz 0.1 0.8 24 256 8 1024 48 model.mpk
+RUSTFLAGS="-C target-cpu=native" cargo run --release -- train train.tar.gz test.tar.gz 0.1 0.8 16 512 8 2048 48 model.mpk

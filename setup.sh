961f173bf340a2a62dc5f2521a24567006aa8598
Elijah Potter <me@elijahpotter.dev>
2026-09-01 08:51:35 -0600

    fix: install tmux as part of setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 apt update -y
-apt install curl build-essential vim htop -y
+apt install curl build-essential vim htop tmux -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"

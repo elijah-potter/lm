542a281e468eca52bc1bafbc50d8321b514c09c6
Elijah Potter <me@elijahpotter.dev>
2026-09-02 09:53:27 -0600

    feat: use batched transfers

--- src/batcher.rs
+++ src/batcher.rs
@@ -1,7 +1,9 @@
 use burn::Tensor;
+use burn::backend::NdArray;
 use burn::data::dataloader::batcher::Batcher;
 use burn::prelude::Backend;
-use burn::tensor::Int;
+use burn::tensor::backend::BackendTypes;
+use burn::tensor::{Device, Int};
 use rand::Rng;
 
 use crate::tokenizer::text_to_indices;
@@ -12,6 +14,10 @@ impl<B: Backend> Batcher<B, Vec<char>, BatchItem<B>> for GenBatcher {
     fn batch(&self, items: Vec<Vec<char>>, device: &B::Device) -> BatchItem<B> {
         let mut inputs = Vec::with_capacity(items.len());
         let mut targets = Vec::with_capacity(items.len());
+
+        // Allows fewer transfers to the GPU. Do just one big one.
+        type CpuBackend = NdArray<f32, i32>;
+        let cpu_device: <CpuBackend as BackendTypes>::Device = Default::default();
 
         for item in items {
             if item.is_empty() {
@@ -24,8 +30,8 @@ impl<B: Backend> Batcher<B, Vec<char>, BatchItem<B>> for GenBatcher {
                 continue;
             }
 
-            let input = text_to_indices(&item[..item.len() - 1], device);
-            let target = text_to_indices(&item[1..], device);
+            let input = text_to_indices::<CpuBackend>(&item[..item.len() - 1], &cpu_device);
+            let target = text_to_indices::<CpuBackend>(&item[1..], &cpu_device);
 
             inputs.push(input);
             targets.push(target);
@@ -35,8 +41,8 @@ impl<B: Backend> Batcher<B, Vec<char>, BatchItem<B>> for GenBatcher {
         let target_tens = Tensor::cat(targets, 0);
 
         BatchItem {
-            input: input_tens,
-            target: target_tens,
+            input: Tensor::<B, 2, Int>::from_data(input_tens.into_data(), device),
+            target: Tensor::<B, 2, Int>::from_data(target_tens.into_data(), device),
         }
     }
 }

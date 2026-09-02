226da72ab1f637e5a69ae26073cdae3f28e24440
Elijah Potter <me@elijahpotter.dev>
2026-09-02 14:40:31 -0600

    fix: update training params

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-RUSTFLAGS="-C target-cpu=native" cargo run --release -- train train.tar.gz test.tar.gz 0.1 0.8 16 512 8 2048 48 model.mpk
+RUSTFLAGS="-C target-cpu=native" cargo run --release -- train train.tar.gz test.tar.gz 0.1 0.04 19 512 8 2176 48 model.mpk
